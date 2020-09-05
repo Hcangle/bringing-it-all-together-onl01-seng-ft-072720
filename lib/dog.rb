a154e89ff7f848be0ca5eadc7a278f53ce410e40
Hannah Angle <hannahcangle@gmail.com>
2020-09-05 00:09:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,14 @@ class Dog
     @id = id 
   end 
   
+  def self.create_table 
+    sql <<-SQL 
+     DROP TABLE IF EXISTS dogs, 
+     id INTEGER PRIMARY KEY 
+     name TEXT 
+     breed TEXT
+  SQL
+end 
+     
+  
 end
