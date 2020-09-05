4f7c8cedd3c4ff87d1f6c1b0871a3cf603c18a49
Hannah Angle <hannahcangle@gmail.com>
2020-09-05 00:06:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,10 @@ class Dog
   attr_accessor :name, :breed, :id
   
   
-  def initialize(name, breed, id=nil)
-    @id = id 
+  def initialize(name:, breed:, id: nil)
     @name = name 
     @breed = breed 
+    @id = id 
   end 
   
 end
