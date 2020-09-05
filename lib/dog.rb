e80c89dfe6da7e086e9f249667d5c069d1fb38a9
Hannah Angle <hannahcangle@gmail.com>
2020-09-05 00:03:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-C
+class Dog 
+  
+  attr_accessor :name, :breed 
+  attr_reader :id 
+  
+  def initialize(name, breed, id=nil)
+    @id = id 
+    @name = name 
+    @breed = breed 
+  end 
+  
+  
