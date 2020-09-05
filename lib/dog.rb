abb0fe99456bf0b022f4fa549ac8d2d00334041f
Hannah Angle <hannahcangle@gmail.com>
2020-09-05 00:35:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,10 +57,17 @@ end
    end 
      
    def self.find_by_id(id)
-     sql = "SELECT * FROM dog WHERE id = ?"
-     result = DB[:conn].execute(sql, id)[0]
-     Dog.new(result[0], result[1], result[2])
-   end 
+     sql = <<-SQL
+     SELECT *
+     FROM dogs
+     WHERE id = ?
+     LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
      
    
    
